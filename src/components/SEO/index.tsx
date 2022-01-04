387c1a59ad09452b7fe013ce2a134d22d6efd788
duong <duongtt@sphinx.com>
2022-01-04 14:26:27 +0700

    add meta seo with img

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export interface SEOProps {
   title: string;
@@ -11,11 +11,16 @@ const SEO = (props: SEOProps) => {
   return (
     <Head>
       <title>{title}</title>
-      {/* <link rel="shortcut icon" href="/access/logo/short-logo.svg" /> */}
+      <link rel="shortcut icon" href="/access/logo/short-logo.svg" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content={description} />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/level-up-tutorials/image/upload/v1628268484/social-cards/default.png"
+        data-svelte="svelte-1u4aq0n"
+      />
     </Head>
-  )
-}
+  );
+};
 
 export default SEO;
